b472db23fb3e0c7be18bceb203168425bf77793c
Yurika Hokama <yurihoka@gmail.com>
2025-01-20 10:51:06 +0900

    Navigation barコンポーネント作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/layouts/Header";
+import NavigationBar from "./components/layouts/NavigationBar";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Header />
+        <NavigationBar />
       </main>
     </div>
   );
