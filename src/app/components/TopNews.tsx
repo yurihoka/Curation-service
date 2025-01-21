54e382644e223e6228f9eca6988314f3aaaefb84
Yurika Hokama <yurihoka@gmail.com>
2025-01-21 22:20:59 +0900

    レイアウト修正

--- src/app/components/TopNews.tsx
+++ src/app/components/TopNews.tsx
@@ -4,10 +4,22 @@ import FetchData from "./FetchData";
 
 export default function TopNews() {
   return (
-    <>
-      <FetchData path="topstories.json?print=pretty" title="TOP" limit={2} />
-      <FetchData path="topstories.json?print=pretty" title="best" limit={2}/>
-      <FetchData path="askstories.json?print=pretty" title="The Latest" limit={2}/>
-    </>
+    <div className="space-y-8">
+      <div className="w-full bg-gray-100 p-8">
+        <FetchData path="topstories.json?print=pretty" title="TOP" limit={1} />
+      </div>
+      <div className="flex justify-between space-x-8">
+        <FetchData
+          path="beststories.json?print=pretty"
+          title="best"
+          limit={2}
+        />
+        <FetchData
+          path="newstories.json?print=pretty"
+          title="The Latest"
+          limit={5}
+        />
+      </div>
+    </div>
   );
 }
