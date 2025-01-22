2778b582cf72ed9a42269c2274476855918db99d
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-01-22 04:50:01 -0800

    記事の大枠のボーダー追加 (#59)

--- src/app/components/layouts/NavigationBar.tsx
+++ src/app/components/layouts/NavigationBar.tsx
@@ -4,8 +4,8 @@ const NavigationBar = () => {
   return (
     <div>
       <nav className="bg-white border-gray-200">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
+        <div className="flex flex-wrap justify-between items-center p-6">
+          <span className="self-center text-4xl font-semibold whitespace-nowrap text-black">
             Hacker News
           </span>
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
@@ -25,7 +25,7 @@ const NavigationBar = () => {
         </div>
       </nav>
       <nav className="border-t border-b border-black">
-        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+        <div className="px-4 py-3">
           <div className="flex items-center">
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
