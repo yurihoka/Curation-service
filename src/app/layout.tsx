118f14415073e76261e10e946f6c2638b12a0df7
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-01-22 01:05:57 -0800

    footerコンポーネント作成 (#53)
    * footerコンポーネント作成
    
    * footerとmain部分が重ならないようにpadding追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/layouts/Header";
 import NavigationBar from "./components/layouts/NavigationBar";
+import Footer from "./components/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Header />
         <NavigationBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
