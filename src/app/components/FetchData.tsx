84146795c8a87149b030ae7f661e58c407b1b367
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-01-22 18:29:33 -0800

    ローディングアニメーション追加 (#71)

--- src/app/components/FetchData.tsx
+++ src/app/components/FetchData.tsx
@@ -82,7 +82,9 @@ export default function FetchData({
   return (
     <>
       {stories === null ? (
-        <p>Loading...</p>
+        <div className="flex justify-center" aria-label="読み込み中">
+          <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+        </div>
       ) : (
         <>
           {category === "The Latest" ? (
