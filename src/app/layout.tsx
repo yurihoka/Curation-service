03dd7511be97390adf14b77e482eb595a62e3a57
Yurika Hokama <125883144+yurihoka@users.noreply.github.com>
2025-01-22 18:06:14 -0800

    フォント変更 (#69)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/layouts/Header";
 import NavigationBar from "./components/layouts/NavigationBar";
 import Footer from "./components/layouts/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className}  antialiased`}>
         <Header />
         <div className="mx-auto max-w-screen-xl border-x border-black text-black">
           <NavigationBar />
